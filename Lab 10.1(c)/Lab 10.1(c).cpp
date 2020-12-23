85530ddfa8f3088c037cb74e5c5572c990f9e77c
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2020-12-23 12:06:39 +0200

    first commit

---
+++ Lab 10.1(c)/Lab 10.1(c).cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int Count(char* str)
+{
+	if (strlen(str) < 3)
+		return 0;
+	int k = 0;
+	for (int i = 0; str[i] != 0; i++)
+		if (str[i] == '+' || str[i] == '-' || str[i] == '=')
+			k++;
+	return k;
+}
+int main()
+{
+	FILE* t;
+	char ch;
+	char str[101];
+	
+	if ((t = fopen("1.txt", "r")) == NULL)
+	{
+		cerr << "Error opening file '" << "1.txt" << "'" << endl;
+		exit(1);
+	}
+	int i= 0;
+	do {
+		ch = getc(t);
+		str[i] = ch;
+		i++;
+	} while (ch != EOF);
+	cout <<"Count +,-,= :"<< Count(str);
+	
+
+	fclose(t);
+	return 0;
+
+}
+
+
